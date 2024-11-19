99185ae2edec6a442043194a23ac61b37e5dd1ba
Daniel-Loi <loid1@mcmaster.ca>
2024-11-18 22:43:49 -0500

    Transition addition

---
+++ audiomix.py
@@ -0,0 +1,17 @@
+
+from pydub import AudioSegment
+from volume import *
+import array
+
+def mix_audio(file1, file2, transition_point1, transition_point2, vol=1, output_file="mixed_song.mp3"):
+    song1 = AudioSegment.from_file(file1)
+    song2 = AudioSegment.from_file(file2)
+    part1 = song1[:int(transition_point1 * 1000)]
+    part2 = song2[int(transition_point2 * 1000):]
+    faded_pt1 = fade_out(part1.get_array_of_samples(),part1.frame_rate,2)
+    faded_pt1 = array.array(song1.array_type, volume(faded_pt1,vol))
+    faded_pt2 = fade_in(part2.get_array_of_samples(),part2.frame_rate,2)
+    faded_pt2 = array.array(song2.array_type, volume(faded_pt2,vol))
+    mixed = song1._spawn(faded_pt1) + song2._spawn(faded_pt2)
+    mixed.export(output_file, format="mp3")
+    print(f"Mixed audio saved as {output_file}")
--- volume.py
+++ volume.py
@@ -10,6 +10,7 @@ def volume(signal,volume):
     return new_signal
 
 def fade_in(signal,rate,duration):
+    signal = np.array(signal).astype(np.float32)
     fade_in_samples = int(duration * rate)
     fade_in_mask = np.linspace(0, 1, fade_in_samples)
     signal[:fade_in_samples] *= fade_in_mask[:, np.newaxis] if signal.ndim > 1 else fade_in_mask
@@ -17,40 +18,42 @@ def fade_in(signal,rate,duration):
     return signal
     
 def fade_out(signal,rate,duration):
+    signal = np.array(signal).astype(np.float32)
     fade_out_samples = int(duration * rate)
     fade_out_mask = np.linspace(1, 0, fade_out_samples)
     signal[-fade_out_samples:] *= fade_out_mask[:, np.newaxis] if signal.ndim > 1 else fade_out_mask
 
     return signal
 
+def normalize_audio(audio, target_rms):
+    current_rms = np.sqrt(np.mean(audio**2))
+    scaling_factor = target_rms / current_rms
+    return audio * scaling_factor
 
-s_rate, signal = wavfile.read("Test.wav") 
+def FFT_graph(signal,sample):
+    signal = signal.astype(np.float32)
+    song_dur = len(signal)/sample   
 
-
-signal = signal.astype(np.float32)
-song_dur = len(signal)/s_rate
-
-FFT = abs(fft(signal))
-freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
-
-
-plt.plot(freqs[range(len(FFT)//2)], FFT[range(len(FFT)//2)])                                                          
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-plt.show()
+    FFT = abs(fft(signal))
+    freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))   
 
 
-new_signal = fade_in(signal,s_rate,10)
-new_signal = fade_out(new_signal,s_rate,20)
-new_signal = volume(new_signal,2)
-
-wavfile.write('output.wav', s_rate, new_signal)
-
-FFT = abs(fft(new_signal))
-freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
+    plt.plot(freqs[range(len(FFT)//2)], FFT[range(len(FFT)//2)])
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude')
+    plt.show()
 
 
-plt.plot(freqs[range(len(FFT)//2)], FFT[range(len(FFT)//2)])  
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-plt.show()
+
+#s_rate, signal = wavfile.read("Test.wav") 
+#                                                        
+#FFT_graph(signal,s_rate)
+#
+#new_signal = normalize_audio(signal,30)
+#new_signal = fade_in(new_signal,s_rate,3)
+#new_signal = fade_out(new_signal,s_rate,3)
+#new_signal = volume(new_signal,1)
+#
+#wavfile.write("output.wav", s_rate, new_signal)
+#
+#FFT_graph(new_signal,s_rate)
